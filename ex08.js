721a87dbe8f03943d80c015faa2f19e8ba7d9c4c
ZxineReal <stargolem90@gmail.com>
2025-03-03 08:11:19 +0700

    upload

--- ex08.js
+++ ex08.js
@@ -5,7 +5,7 @@ let c = Number(prompt("Mời bạn nhập vào cạnh thứ ba của tam giác: 
 if ((a + b > c) || ( a + c > b) || (b + c > a)) {
     if (a == b && a == c) {
         console.log("Đây là tam giác đều");
-    } else if ((a == b) || (b == c) || (c == a)){
+    } else if ((a == b) && (b == c) && (c == a)){
         console.log("Đây là tam giác cân");
     } else if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == b * b + a * a)) {
         console.log("Đây là tam giác vuông");
